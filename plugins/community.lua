10f1f72ab4da60b04786d4344ae1b8ae33534b4d
Asrorbek Mannonov <asrorbekmannonov2016@gmail.com>
2023-03-20 11:59:26 +0500

    updates

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,60 @@
-return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+-- return {
+--   -- Add the community repository of plugin specifications
+--   "AstroNvim/astrocommunity",
+--   -- example of imporing a plugin, comment out to use it or add your own
+--   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+--
+--   -- { import = "astrocommunity.colorscheme.catppuccin" },
+--   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+-- }
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.nightfox",  enabled = false },
+  { import = "astrocommunity.colorscheme.kanagawa",  enabled = true },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = true,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+    },
+  },
+  { import = "astrocommunity.completion.copilot-lua" },
+  {
+    -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-L>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 100,
+      disabled_filetypes = { "help" },
+    },
+  },
 }
